73408b6442baab2f5f46676de955061aaed4d64c
Mukesh-Khemani <mukeshkhemani222@gmail.com>
2021-08-19 22:40:13 +0530

    Building Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
-
+import React from "react";
+import{ BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Header from "./Header"
 function App() {
   return (
-    <div className="App">
-      <p>Amazon Clone</p>
+    <Router>
+    <div className="app">
+      <Switch>
+        <Route path="/checkout">
+          <h1>Checkout</h1>
+        </Route>
+        <Route path="/login">
+          <h1>Login Page</h1>
+        </Route>
+        
+        <Route path="/">
+          <Header />
+          <h1>HOME PAGE!!!</h1>
+        </Route>
+        
+      </Switch>
+     
     </div>
+    </Router>
   );
 }
 
