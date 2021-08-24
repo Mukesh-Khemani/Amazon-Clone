acb616fdfea2f71cdcec65c0798524abf67b1441
Mukesh-Khemani <mukeshkhemani222@gmail.com>
2021-08-24 16:45:02 +0530

    Almost Finished Making Product Components

--- src/Home.js
+++ src/Home.js
@@ -9,13 +9,54 @@ function  Home() {
             src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
             alt=""
             /> 
-            <Product
-            id="1234567"
-            title="Mark Twain "
-            price={12.00}
-            rating={4.4}
-            image="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.in%2FGreatest-Novels-Twain-Deluxe-Hardbound%2Fdp%2F9388810457&psig=AOvVaw3uTkxxvwL4fPCNdSV65Z8I&ust=1629829203198000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIjFzonhx_ICFQAAAAAdAAAAABAD"
-            />
+            <div className="home__row">
+                <Product
+                id="1234567"
+                title="The Adventures of Tom Sawyer:Mark Twain "
+                price={10.50}
+                rating={5}
+                image="https://m.media-amazon.com/images/I/51ONaww2BhL.jpg"
+                />
+                <Product
+                id="1234567"
+                title="The Adventures of Tom Sawyer:Mark Twain "
+                price={10.50}
+                rating={5}
+                image="https://m.media-amazon.com/images/I/51ONaww2BhL.jpg"
+                />
+            </div>
+            <div className="home__row">
+                <Product
+                id="1234567"
+                title="The Adventures of Tom Sawyer:Mark Twain "
+                price={10.50}
+                rating={5}
+                image="https://m.media-amazon.com/images/I/51ONaww2BhL.jpg"
+                />
+                <Product
+                id="1234567"
+                title="The Adventures of Tom Sawyer:Mark Twain "
+                price={10.50}
+                rating={5}
+                image="https://m.media-amazon.com/images/I/51ONaww2BhL.jpg"
+                />
+                 <Product
+                id="1234567"
+                title="The Adventures of Tom Sawyer:Mark Twain "
+                price={10.50}
+                rating={5}
+                image="https://m.media-amazon.com/images/I/51ONaww2BhL.jpg"
+                />
+            </div>
+            <div classNmae="home__row">
+                <Product
+                id="1234567"
+                title="The Adventures of Tom Sawyer:Mark Twain "
+                price={10.50}
+                rating={5}
+                image="https://m.media-amazon.com/images/I/51ONaww2BhL.jpg"
+                />
+            </div>
         </div>
     );
 }
